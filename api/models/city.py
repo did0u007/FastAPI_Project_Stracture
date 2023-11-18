2c767c2f3e78a39023b24a76d164c0ccf47371ad
did0u007 <mr.did0u007@gmail.com>
2023-11-18 16:09:20 +0100

    befor loading from main

--- api/models/city.py
+++ api/models/city.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import relationship
 
 class City(Base):
     __tablename__ = "cities"
-    id = Column("id", Integer, primary_key=True)
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
     name = Column("name", Unicode, index=True, nullable=False)
     state_id = Column("states_id", Integer, ForeignKey("states.id"))
     state = relationship("State", foreign_keys=state_id)
